63b122365805708e5be98532bdf9550ea9da753f
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-07-30 08:04:20 +0530

    Create a new action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,25 @@ class PortfoliosController < ApplicationController
    def index
     @items = Portfolio.all
    end
+
+   def new
+   @item = Portfolio.new
+   end
+
+
+   def create
+  @item = Portfolio.new(portfolio_params)
+   if @item.save
+       redirect_to  portfolios_path
+   else
+      render 'new'
+   end
+   end
+
+    private
+
+    def portfolio_params
+       params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
+
 end

6fab1b42fc7f6c90f95a68e31fe9b15c5699fb0e
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-07-31 19:29:31 +0530

    Friendly_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_display' # CUSTOM ROUTES FOR resources
   root to: 'pages#home'
-
   get '/about', to: 'pages#about'
   get '/contact',to: 'pages#contact'
-
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

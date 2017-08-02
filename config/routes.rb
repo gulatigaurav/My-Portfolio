0a1000eadad2a27be6d6e3eb8aa6406076768e35
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-08-02 20:03:26 +0530

    Implemented validations

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :blogs do
     member do
        get :toggle_status
-    end
+     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

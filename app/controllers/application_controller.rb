927e86035ad89093bf9333ce0dde102f03562457
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-08-05 11:40:53 +0530

    Refactored application controller to include concerns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,10 @@ class ApplicationController < ActionController::Base
       #    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
       # end
    include DeviseWhitelist  # concerns in rails while using name as an additional controller
-   before_action :set_source
-
-    def set_source
-     session[:source]= params[:q] if params[:q]
-    end
+   include SetSource
+  #  before_action :set_source
+    # def set_source
+    #  session[:source]= params[:q] if params[:q]
+    # end
 
 end

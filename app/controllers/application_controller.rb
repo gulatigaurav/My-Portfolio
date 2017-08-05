14149b47dda3b5b07f5a18d6fd216c58480b996a
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-08-05 07:49:50 +0530

    Added custom Attributes best practices

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-      before_filter :configure_permitted_paramaters, if: :devise_controller?
-
-      def configure_permitted_paramaters
-         devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-         devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-      end
+      # before_filter :configure_permitted_paramaters, if: :devise_controller?
+      #  Not considered as the best practice 5
+      #  As each should be allowed only one responsibility
+      # def configure_permitted_paramaters
+      #    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      #    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      # end
+   include DeviseWhitelist
 end

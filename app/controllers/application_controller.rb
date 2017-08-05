33de0aaeae1a0c001d98f369d9756bdb6e6ff9c7
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-08-05 12:57:03 +0530

    Added guest user for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-      # before_filter :configure_permitted_paramaters, if: :devise_controller?
-      #  Not considered as the best practice 5
-      #  As each should be allowed only one responsibility
-      # def configure_permitted_paramaters
-      #    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      #    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-      # end
    include DeviseWhitelist  # concerns in rails while using name as an additional controller
    include SetSource
-  #  before_action :set_source
-    # def set_source
-    #  session[:source]= params[:q] if params[:q]
-    # end
-
+   include CurrentUserConcern
+  # def current_user
+  #   super || OpenStruct.new( name: "Guest User",
+  #                            first_name: "Guest",
+  #                            last_name: "User",
+  #                            email: "guest@example.com",
+  #                           #  guest: true
+  #                            )
+  #   end
 end
+
+
+
+
+# before_filter :configure_permitted_paramaters, if: :devise_controller?
+#  Not considered as the best practice 5
+#  As each should be allowed only one responsibility
+# def configure_permitted_paramaters
+#    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+#    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+# end
+# //////////////////////
+#  before_action :set_source
+# def set_source
+#  session[:source]= params[:q] if params[:q]
+# end

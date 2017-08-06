d4c22f98c91262c35669b835655feee3a68df432
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-08-06 08:19:15 +0530

    View helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+   def login_helper
+    #  " <p> My Helper </p>".html_safe
+    if current_user.is_a?(User)
+     link_to 'Log out', destroy_user_session_path,
+                                                 method: :delete
+    else
+      (link_to 'Register', new_user_registration_path) + " ".html_safe +
+      (link_to 'Signin',new_user_session_path)
+  end
+ end
 end

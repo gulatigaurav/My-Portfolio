5f79561c8c5844e978b46696a3d681dbcd42ee3e
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-08-27 12:54:21 +0530

    Implement video background

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.precompile += %w( portfolios.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.paths << "#{Rails.root}/app/assets/videos"
